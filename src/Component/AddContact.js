25d7fd0faef15c9201216937b7c3e9b65b0e9ccc
Moinuddin Kamal <IT@pronayonbd.com>
2021-06-03 10:49:54 +0600

    react Apps Create

---
+++ src/Component/AddContact.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class AddContact extends React.Component{
+    render(){
+
+        return(
+            <div className="ui main">
+                <h2> Add Contact</h2>
+                <form className="ui form">
+                    <div className="Field">
+                        <lable>Name</lable>
+                        <input type="text" name="name" placeholder="Name"/>
+                    </div>
+                    <div className="Field">
+                        <lable>Email</lable>
+                        <input type="text" name="Email" placeholder="Email"/>
+                    </div>
+                    <button className="ui button  blue">Add</button>
+                </form>
+            </div>
+        )
+    }
+}
+export default AddContact;
---
+++ src/Component/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import ContactCard from './ContactCard';
+import ContactList from './ContactList';
+import AddContact from './AddContact';
+
+
+function App() {
+  return <div>
+    <Header/>
+    <AddContact/>
+  </div>;
+}
+
+export default App;
